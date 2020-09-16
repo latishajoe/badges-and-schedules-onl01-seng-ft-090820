0bce6e0934a0599283b2ca0916235f74a8c4d376
Latisha Joe <latisha_joe@yahoo.com>
2020-09-16 01:22:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,21 +10,22 @@ def batch_badge_creator(array)
   return new_array
 end
 
-def assign_rooms(arr)
+def assign_rooms(array)
   new_array = [ ]
   counter = 1
-  arr.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  array.each do |name|
+    new_array << ("Hello, #{name}! You'll be assigned to room #{counter}!")
   counter += 1
   end
   return new_array
 end
 
-def printer(arr)
-  batch_badge_creator(arr).each do |badge|
+def printer(array)
+  batch_badge_creator(array).each do |badge|
     puts badge
   end
-  assign_rooms(arr).each do |assignment|
+  assign_rooms(array).each do |assignment|
     puts assignment
   end
 end
+
